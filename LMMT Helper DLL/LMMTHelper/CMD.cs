ad0284d7eba979237bcc1b1d43c036dfd068aa2f
krypto5863 <29824718+krypto5863@users.noreply.github.com>
2021-05-16 18:06:19 -0400

    Updated and Organized Helper DLL

---
+++ LMMT Helper DLL/LMMTHelper/CMD.cs
@@ -0,0 +1,98 @@
+﻿using System.Windows.Forms;
+
+namespace LMMTHelper
+{
+	class CMD
+	{
+		public static bool Run(string command)
+		{
+
+			var res = DialogResult.OK;
+
+			System.Diagnostics.Process process = new System.Diagnostics.Process();
+			System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+			startInfo.FileName = "cmd.exe";
+			startInfo.Arguments = "/c " + command;
+			startInfo.UseShellExecute = false;
+			startInfo.Verb = "runas";
+			startInfo.RedirectStandardOutput = true;
+			startInfo.RedirectStandardError = true;
+			startInfo.CreateNoWindow = true;
+			process.StartInfo = startInfo;
+
+			do
+			{
+
+				res = DialogResult.OK;
+
+				process.Start();
+
+				string line = process.StandardOutput.ReadToEnd();
+				string err = process.StandardError.ReadToEnd();
+#if DEBUG
+            System.Windows.Forms.MessageBox.Show(startInfo.Arguments + "\n\n" + line, "DEBUG", MessageBoxButtons.OK, MessageBoxIcon.Information);
+#endif
+
+				process.WaitForExit();
+
+				if (!err.Equals("") && err != null)
+				{
+					res = System.Windows.Forms.MessageBox.Show("An error was tossed while trying to carry out a command!\n\n" + err + "\n\nClose all open windows and try again, or ignore the error and continue. Otherwise you may abort the installation.", "Error while performing command!", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+
+					if (res == DialogResult.Abort)
+					{
+						return false;
+					}
+				}
+			} while (res == DialogResult.Retry);
+			return true;
+		}
+
+		public static bool Move(string src, string trg)
+		{
+
+			var res = DialogResult.OK;
+
+			System.Diagnostics.Process process = new System.Diagnostics.Process();
+			System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+			startInfo.FileName = "cmd.exe";
+			startInfo.Arguments = "/c move /y " +
+				'\u0022' + src + '\u0022' +
+				" " +
+				'\u0022' + trg + '\u0022';
+
+			startInfo.UseShellExecute = false;
+			startInfo.Verb = "runas";
+			startInfo.RedirectStandardOutput = true;
+			startInfo.RedirectStandardError = true;
+			startInfo.CreateNoWindow = true;
+			process.StartInfo = startInfo;
+
+			do
+			{
+
+				res = DialogResult.OK;
+
+				process.Start();
+
+
+				string err = process.StandardError.ReadToEnd();
+				string line = process.StandardOutput.ReadToEnd();
+				process.WaitForExit();
+#if DEBUG
+            System.Windows.Forms.MessageBox.Show(startInfo.Arguments + "\n\n" + line, "DEBUG", MessageBoxButtons.OK, MessageBoxIcon.Information);
+#endif
+				if (line.Contains("0"))
+				{
+					res = System.Windows.Forms.MessageBox.Show("We failed to move the file at " + src + "\n\n" + err + "\n\nClose all open windows and try again. Otherwise, you can ignore it or abort the installation. Ignoring could cause problems with the installation!", "Error while moving!", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+					if (res == DialogResult.Abort)
+					{
+						return false;
+					}
+				}
+			} while (res == DialogResult.Retry);
+
+			return true;
+		}
+	}
+}
--- LMMT Helper DLL/LMMTHelper/Properties/AssemblyInfo.cs
+++ LMMT Helper DLL/LMMTHelper/Properties/AssemblyInfo.cs
@@ -2,34 +2,34 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to ify the information
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("LMMTHelper")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("LMMTHelper")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("711cc3c2-07db-46ca-b34b-ba06f4edcbcd")]
+[assembly: Guid("d694825e-aa76-42a2-ac93-b5e03d5eea2c")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
